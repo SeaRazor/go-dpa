0d846b8ae5c38c04dddd75ef7780f72073f25a7e
Egor Sidorovich <e.sidorovich@godeltech.com>
2018-11-09 18:38:38 +0300

    endpoint methods added

--- repository/policy_repository.go
+++ repository/policy_repository.go
@@ -2,32 +2,43 @@ package repository
 
 import (
 	"dpa/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
 type PolicyRepository struct {
-	//Config *models.Configuration
+	Config *models.Configuration
 }
 
-func (repository *PolicyRepository) GetPolicy(policyId int) (models.Policy, error) {
-	policy := models.Policy{}
+func (repo *PolicyRepository) GetPolicy(policyId int32) (models.Policy, error) {
+	db, err := gorm.Open("mssql", models.CurrentConfiguration.DatabaseConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	var policy models.Policy
+	result := db.Where(&models.Policy{ID: policyId}).First(&policy)
+	if result.RecordNotFound() {
+	}
+
 	return policy, nil
 }
 
-func (repository *PolicyRepository) GetPolicies(params models.RequestParams) ([]models.Policy, error) {
+func (repo *PolicyRepository) GetPolicies(params models.RequestParams) ([]models.Policy, error) {
 	policies := make([]models.Policy, 10)
 	return policies, nil
 }
 
-func (repository *PolicyRepository) CreatePolicy(policy *models.Policy) (models.Policy, error) {
+func (repo *PolicyRepository) CreatePolicy(policy *models.Policy) (models.Policy, error) {
 	createdPolicy := models.Policy{}
 	return createdPolicy, nil
 }
 
-func (repository *PolicyRepository) UpdatePolicy(id int, policy *models.Policy) (models.Policy, error) {
+func (repo *PolicyRepository) UpdatePolicy(id int, policy *models.Policy) (models.Policy, error) {
 	updatedPolicy := models.Policy{}
 	return updatedPolicy, nil
 }
 
-func (repository *PolicyRepository) DeletePolicy(id int) error {
+func (repo *PolicyRepository) DeletePolicy(id int) error {
 	return nil
 }
